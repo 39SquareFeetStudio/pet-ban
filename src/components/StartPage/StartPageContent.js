82a75ea1509323b064f053e7c9678355d58187e4
rick <edcrfvlily5@gmail.com>
2021-12-18 10:27:43 +0800

    [modify] 首頁排版新增content

--- src/components/StartPage/StartPageContent.js
+++ src/components/StartPage/StartPageContent.js
@@ -2,19 +2,45 @@ import classes from "./StartPageContent.module.css";
 import { ReactComponent as Cat } from "../../assets/cat.svg";
 const StartPageContent = () => {
   return (
-    <section className={classes.starting}>
+    <section className={classes.body}>
       <div className={classes.banner}>
         <div className={classes.content}>
-          <div>
-            <p className={classes.h1}>
-              Pet 伴，
+          <div className={classes.subject}>
+            <h1>
+              Pet 伴
               <br />
               陪伴你的寵物一生
-            </p>
-            <p className={classes.h2}>毛小孩的就診紀錄整合平台</p>
+              <br />
+            </h1>
+            <div className={classes.cosH2}>毛小孩醫療資訊整合平台</div>
           </div>
           <Cat />
         </div>
+      </div>
+      <div className={classes.mainContent}>
+        <h1>我們提供的服務</h1>
+        <div className={classes.span}>飼主</div>
+        <p>
+          提供醫療紀錄
+          <br />
+          醫院預約及查詢掛號狀態
+        </p>
+        <button className={classes.control}>成為會員</button>
+        <div>.........</div>
+        <div className={classes.span}>
+          動物之家
+          <br />
+          中途
+        </div>
+        <p>
+          追蹤送養寵物就診及預防針施打
+          <br />
+          可以於平台上媒合飼養者
+        </p>
+        <button className={classes.control}>成為會員</button>
+      </div>
+      <div className={classes.businessPartners}>
+        <h1>我們合作的動物醫院</h1>
       </div>
     </section>
   );
