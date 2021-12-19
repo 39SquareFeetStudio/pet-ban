216076b4702a82824553b9ccee0586a3d2e9fee3
rick <edcrfvlily5@gmail.com>
2021-12-19 10:30:15 +0800

    [add] 新增divide客制顏色

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    divideColor: (theme) => ({
+      ...theme("borderColors"),
+      primary: "#E4651D",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+    }),
   },
   variants: {
     extend: {},
+  },
+  corePlugins: {
+    divideColor: true,
   },
   plugins: [],
 };
