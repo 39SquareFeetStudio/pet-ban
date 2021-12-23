3c6540320077a6a48ec6d03f8d4868a211878309
rick <edcrfvlily5@gmail.com>
2021-12-23 11:43:44 +0800

    [modify]排版調整

--- src/components/StartPage/StartPageContent.js
+++ src/components/StartPage/StartPageContent.js
@@ -2,7 +2,7 @@ import classes from "./StartPageContent.module.css";
 import { ReactComponent as Cat } from "../../assets/cat.svg";
 const StartPageContent = () => {
   return (
-    <section className={classes.body}>
+    <section className={classes.startBody}>
       <div className={classes.banner}>
         <div className={classes.content}>
           <div className={classes.subject}>
@@ -17,19 +17,21 @@ const StartPageContent = () => {
           <Cat className={classes.logo} />
         </div>
       </div>
-      <div className={classes.mainContent}>
-        <h1>我們提供的服務</h1>
-        <div className={"divide-y-4 divide-primary"}>
-          <div className={classes.mainContent}>
+      <div className={classes.mainBody}>
+        <div className={classes.mainContent}>
+          <h1>我們提供的服務</h1>
+          <div className={classes.mainItem}>
             <div className={classes.span}>飼主</div>
             <p>
               提供醫療紀錄
               <br />
               醫院預約及查詢掛號狀態
             </p>
-            <button className={classes.control}>成為會員</button>
+            <div className={classes.controlBody}>
+              <button className={classes.control}>成為會員</button>
+            </div>
           </div>
-          <div className={classes.mainContent}>
+          <div className={classes.mainItem}>
             <div className={classes.span}>
               動物之家
               <br />
@@ -40,12 +42,19 @@ const StartPageContent = () => {
               <br />
               可以於平台上媒合飼養者
             </p>
-            <button className={classes.control}>成為會員</button>
+            <div className={classes.controlBody}>
+              <button className={classes.control}>成為會員</button>
+            </div>
           </div>
         </div>
       </div>
-      <div className={classes.mainContent}>
-        <div className={classes.span}>我們合作的動物醫院</div>
+      <div className={classes.partnerBody}>
+        <div className={classes.partnerContent}>
+          <h1>我們合作的動物醫院</h1>
+          <ul>
+            <li>台中市：</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
