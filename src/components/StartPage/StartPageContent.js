b7ccea00f23d476b5f2dbcadc6eff9495b2a218a
rick <edcrfvlily5@gmail.com>
2021-12-23 10:11:56 +0800

    [modify] 調色

--- src/components/StartPage/StartPageContent.js
+++ src/components/StartPage/StartPageContent.js
@@ -19,28 +19,33 @@ const StartPageContent = () => {
       </div>
       <div className={classes.mainContent}>
         <h1>我們提供的服務</h1>
-        <div className={classes.span}>飼主</div>
-        <p>
-          提供醫療紀錄
-          <br />
-          醫院預約及查詢掛號狀態
-        </p>
-        <button className={classes.control}>成為會員</button>
-        <div>.........</div>
-        <div className={classes.span}>
-          動物之家
-          <br />
-          中途
+        <div className={"divide-y-4 divide-primary"}>
+          <div className={classes.mainContent}>
+            <div className={classes.span}>飼主</div>
+            <p>
+              提供醫療紀錄
+              <br />
+              醫院預約及查詢掛號狀態
+            </p>
+            <button className={classes.control}>成為會員</button>
+          </div>
+          <div className={classes.mainContent}>
+            <div className={classes.span}>
+              動物之家
+              <br />
+              中途
+            </div>
+            <p>
+              追蹤送養寵物就診及預防針施打
+              <br />
+              可以於平台上媒合飼養者
+            </p>
+            <button className={classes.control}>成為會員</button>
+          </div>
         </div>
-        <p>
-          追蹤送養寵物就診及預防針施打
-          <br />
-          可以於平台上媒合飼養者
-        </p>
-        <button className={classes.control}>成為會員</button>
       </div>
-      <div className={classes.businessPartners}>
-        <h1>我們合作的動物醫院</h1>
+      <div className={classes.mainContent}>
+        <div className={classes.span}>我們合作的動物醫院</div>
       </div>
     </section>
   );
