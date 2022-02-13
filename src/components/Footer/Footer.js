780bb6984775efb6f8bec40ec5dcac688df9a9cf
rick <edcrfvlily5@gmail.com>
2022-02-13 14:10:18 +0800

    [modify]工作室名稱更正

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <button>合作夥伴</button>
       </div>
       <div className={classes.content}>
-        <p>Copyright &copy; 2021 GooCat谷科工作室</p>
+        <p>Copyright &copy; 2021 PetBan創意工作室</p>
       </div>
     </div>
   );
