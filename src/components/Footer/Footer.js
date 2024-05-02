8ca845dca0575eb5a1c28d373f618a7c7a52409d
Rickluo94 <edcrfvlily5@gmail.com>
2024-05-02 15:05:44 +0800

    [modify] 轉為開源項目調整

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,27 @@
 import classes from "./Footer.module.css";
 
 const Footer = () => {
+
+
   return (
     <div className={classes.footerBody}>
       <div className={classes.footerMain}>
-        <button>關於我們</button>
-        <button>意見回饋</button>
-        <button>合作夥伴</button>
+        <a href={"https://www.tasker.com.tw/workroom/11WXE7"}
+            target={"_blank"}
+            rel="noreferrer">
+          <button>關於我們</button>
+        </a>
+        <a>
+          <button>提案</button>
+        </a>
+        <a href={"https://wilddonkeyworks.myportfolio.com/"}
+            target={"_blank"}
+            rel="noreferrer">
+          <button>合作夥伴</button>
+        </a>
       </div>
       <div className={classes.content}>
-        <p>Copyright &copy; 2021 PetBan創意工作室</p>
+        <p>Copyright &copy; 2021 山石久平工作室</p>
       </div>
     </div>
   );
